68427539b10c06002ce210f28421cf70bbdad6d1
Heli Eronen <herojonen@gmail.com>
2023-01-10 13:59:46 +0200

    Write consumers as asynchronous

--- backend/birdnest/consumers.py
+++ backend/birdnest/consumers.py
@@ -1,25 +1,24 @@
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-class PilotListConsumer(WebsocketConsumer):
+class PilotListConsumer(AsyncWebsocketConsumer):
 
-    def connect(self):
+    async def connect(self):
         self.room_name = 'pilot_list'
         self.room_group_name = 'pilot_list'
 
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, event):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, event):
+        await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
 
-    def pilot_data(self, event):
+    async def pilot_data(self, event):
         data = event["data"]
 
-        self.send(text_data=json.dumps({"data": data}))
+        await self.send(text_data=json.dumps({"data": data}))
